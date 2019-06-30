dc55960514a7710e5a7dd592770c20e1e5416f7b
LIParadise <a58524andy@gmail.com>
2019-06-30 22:31:27 +0800

    modification by MM

--- source/main.cpp
+++ source/main.cpp
@@ -1,28 +1,23 @@
-#include <events/mbed_events.h>
 #include <mbed.h>
 #include "ble/BLE.h"
-#include "ble/gap/Gap.h"
-#include "ble/services/HeartRateService.h"
 #include "pretty_printer.h"
-#include "USBHostMSD.h"
-#include "USBHostMouse.h"
 #include "USBHostKeyboard.h"
-#include "FATFileSystem.h"
-#include "HIDService.h"
+#include "KeyboardService.h"
+#include "examples_common.h"
 #include "DeviceInformationService.h"
+#include <events/mbed_events.h>
 
-const static char DEVICE_NAME[] = "USB Device";
+
+const static char DEVICE_NAME[] = "HID Device";
+static const uint16_t uuid16_list[] = {GattService::UUID_HUMAN_INTERFACE_DEVICE_SERVICE};
 
 static events::EventQueue event_queue(/* event count */ 16 * EVENTS_EVENT_SIZE);
 
 class USB_Device ;
 
-static const uint16_t uuid16_list[]        = {GattService::UUID_HUMAN_INTERFACE_DEVICE_SERVICE};
-static char msg[25] = {'\0'};
-static uint8_t key_press_scan_buff[50] = {'\0'};
-static uint8_t modifyKey[50] = {'\0'};
-static char keyData;
 USB_Device* demoPtr = NULL;
+
+void disconnectionCallback(const Gap::DisconnectionCallbackParams_t *params);
 
 void passkeyDisplayCallback(Gap::Handle_t handle, const SecurityManager::Passkey_t passkey)
 {
@@ -72,9 +67,9 @@ class USB_Device : ble::Gap::EventHandler {
       _ble.gap().accumulateAdvertisingPayload(GapAdvertisingData::COMPLETE_LOCAL_NAME, (uint8_t *)DEVICE_NAME, sizeof(DEVICE_NAME));
       _ble.gap().accumulateAdvertisingPayload(GapAdvertisingData::KEYBOARD);
       _ble.gap().accumulateAdvertisingPayload(GapAdvertisingData::BREDR_NOT_SUPPORTED | GapAdvertisingData::LE_GENERAL_DISCOVERABLE);
-      _ble.gap().accumulateAdvertisingPayload(GapAdvertisingData::COMPLETE_LIST_16BIT_SERVICE_IDS, (uint8_t *)uuid16_list, sizeof(uuid16_list));
+      _ble.gap().accumulateAdvertisingPayload(GapAdvertisingData::COMPLETE_LIST_16BIT_SERVICE_IDS, (uint8_t *)uuid16_list, sizeof(_hid_uuid));
       _ble.gap().setAdvertisingType(GapAdvertisingParams::ADV_CONNECTABLE_UNDIRECTED);
-      _ble.gap().setAdvertisingInterval(1000);
+      _ble.gap().setAdvertisingInterval(50);
 
       _event_queue.call_every(500, this, &USB_Device::blink);
 
@@ -85,18 +80,22 @@ class USB_Device : ble::Gap::EventHandler {
 
     }    
 
-    void update_keyboard_value(int i) {
-      if (_connected) {
-        _hid_service.updateReport(modifyKey[i], key_press_scan_buff[i]);
-      }
-    }
-
     bool connected() {
       return _ble.getGapState().connected;
     }
 
     void public_startAdvertising(){
       start_advertising();
+    }
+    
+    void send_string(const char * c) {    
+        if (!_connected) {
+            HID_DEBUG("we haven't connected yet...");
+        } else {
+            int len = strlen(c);
+            _hid_service.printf(c);
+            HID_DEBUG("sending %d chars\r\n", len);
+        }
     }
 
   private:
@@ -120,6 +119,8 @@ class USB_Device : ble::Gap::EventHandler {
     void blink(void) {
       _led1 = !_led1;
     }
+    
+
 
   private:
     /* Event handler */
@@ -146,7 +147,7 @@ class USB_Device : ble::Gap::EventHandler {
 
     UUID _hid_uuid;
 
-    HIDService _hid_service;
+    KeyboardService _hid_service;
 
     DeviceInformationService _deviceInfo;
 
@@ -154,7 +155,6 @@ class USB_Device : ble::Gap::EventHandler {
     ble::AdvertisingDataBuilder _adv_data_builder;
 };
 
-/** Schedule processing of events from the BLE middleware in the event queue. */
 void schedule_ble_events(BLE::OnEventsToProcessCallbackContext *context) {
   event_queue.call(Callback<void()>(&context->ble, &BLE::processEvents));
 }
@@ -166,76 +166,9 @@ void disconnectionCallback(const Gap::DisconnectionCallbackParams_t *params)
 
 void onKey(uint8_t key)
 {
-  printf("Key: %c\r\n", key);
-
-  int index_w, index_b;
-  index_w = index_b = 0;
-
-  keyData = key;
-
-  msg[index_w++] = keyData;
-  if (keyData <= 0x39 && keyData >= 0x30)
-  { //number
-    if (keyData == 0x30)
-    {
-      modifyKey[index_b] = 0x00;
-      key_press_scan_buff[index_b] = 0x27;
-      index_b++;
-      key_press_scan_buff[index_b] = 0x73;
-    }
-    else
-    {
-      modifyKey[index_b] = 0x00;
-      key_press_scan_buff[index_b] = keyData - 0x13;
-      index_b++;
-      key_press_scan_buff[index_b] = 0x73;
-    }
-  }
-  else if (keyData <= 0x7a && keyData >= 0x61)
-  { //lowercase letters
-    modifyKey[index_b] = 0x00;
-    key_press_scan_buff[index_b] = keyData - 0x5d;
-    index_b++;
-    key_press_scan_buff[index_b] = 0x73;
-  }
-  else if (keyData <= 0x5a && keyData >= 0x41)
-  { //uppercase letters
-    modifyKey[index_b] = 0x02;
-    key_press_scan_buff[index_b] = keyData - 0x3d;
-    index_b++;
-    key_press_scan_buff[index_b] = 0x73;
-  }
-  else if (keyData == 0x20)
-  { //space
-    modifyKey[index_b] = 0x00;
-    key_press_scan_buff[index_b] = 0x2c;
-    index_b++;
-    key_press_scan_buff[index_b] = 0x73;
-  }
-  else
-  {
-    modifyKey[index_b] = 0x00;
-    //key_press_scan_buff[index_b] = 0x73;          //this is dummy data.
-    //msg[index_w+1] = '\0';
-  }
-
-  // TODO 
-
-  index_b++;
-
-  if (keyData == 0x0a && demoPtr != NULL && demoPtr -> connected())
-  {
-    for (int i = 0; i < index_b; i++)
-    {
-      demoPtr -> update_keyboard_value(i);
-      wait(0.03);
-    }
-    index_b = 0;
-    index_w = 0;
-    memset(modifyKey, 0, 50);
-    memset(msg, 0, 25);
-    memset(key_press_scan_buff, 0, 50);
-  }
+  printf("Key: %c\r\n", key);  
+  char * keydata = reinterpret_cast<char*>(&key);
+  demoPtr->send_string(keydata);
 }
 
 
@@ -263,6 +196,7 @@ int main()
 {
 
   Thread keyboardTask(keyboard_task, NULL, osPriorityNormal, 1024 * 4);
+
   BLE &ble = BLE::Instance();
   ble.onEventsToProcess(schedule_ble_events);
   USB_Device demo(ble, event_queue);
@@ -273,4 +207,3 @@ int main()
 
   return 0;
 }
-
